f2f6bc294b051111ae493f1518fc46508434658b
Anshuman <chaoanxuman999@gmail.com>
2025-06-23 20:09:53 +0530

    Conqest in progress

---
+++ src/main/java/com/conqest/conq/ConqApplication.java
@@ -0,0 +1,13 @@
+package com.conqest.conq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conqest/conq/ConqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conqest.conq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
